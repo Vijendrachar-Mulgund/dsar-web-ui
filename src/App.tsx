fa4d0133baf3797c324d830ed6e8def8b396e3bf
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-07-21 23:46:34 +0100

    Router is configured ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import { Button } from "@/components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <h1>Home One</h1>,
+    },
+    {
+      path: "/login",
+      element: <h1>Login</h1>,
+    },
+    {
+      path: "/profile",
+      element: <h1>Profile</h1>,
+    },
+    {
+      path: "/dashboard",
+      element: <h1>Dashboard</h1>,
+    },
+    {
+      path: "*",
+      element: <h1>Not Found</h1>,
+    },
+  ]);
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button variant="destructive">Destructive</Button>
+
+      <RouterProvider router={router} />
     </div>
   );
 }
