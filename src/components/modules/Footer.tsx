8257222836c752386c07765c15a79b42c58a63eb
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-07-24 12:19:05 +0100

    Theme changed ✅

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-background py-6 w-full">
+    <footer className="bg-background py-6 w-full fixed bottom-0">
       <div className="container max-w-7xl flex flex-col items-center justify-center gap-4 sm:flex-row">
         <p className="text-xs text-muted-foreground">&copy; 2024 DSaR. All rights reserved.</p>
         <nav className="flex gap-4 sm:gap-6"></nav>
