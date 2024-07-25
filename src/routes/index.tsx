6d0242735ec1e37e01d9f97ef226f53f520e2019
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-07-25 14:39:09 +0100

    All the routes are configured ✅

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,11 @@ import App from "@/App";
 import { Admin } from "@/pages/admin";
 import { CreateNewAccount } from "@/pages/admin/create-new-account";
 import { Dashboard } from "@/pages/admin/dashboard";
+import { Case } from "@/pages/case";
+import { CaseDetail } from "@/pages/case/case-detail";
+import { CaseList } from "@/pages/case/case-list";
 import { Login } from "@/pages/login";
+import { NotFound } from "@/pages/not-found";
 import { createBrowserRouter } from "react-router-dom";
 
 export const routes = createBrowserRouter([
@@ -27,6 +31,25 @@ export const routes = createBrowserRouter([
             element: <Dashboard />,
           },
         ],
+      },
+      {
+        path: "case",
+        element: <Case />,
+        children: [
+          {
+            path: "list",
+            element: <CaseList />,
+          },
+
+          {
+            path: "detail/:caseId",
+            element: <CaseDetail />,
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: <NotFound />,
       },
     ],
   },
