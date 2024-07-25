f7bc062e031e6914b293c4d5577169bc1e112203
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-07-25 19:09:19 +0100

    Theme provider code refactored ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "./components/modules/Header";
-import { Footer } from "./components/modules/Footer";
-import { ThemeProvider } from "./components/theme/ThemeProvider";
-import { ScrollArea } from "./components/ui/scroll-area";
+import { Header } from "@/components/modules/Header";
+import { Footer } from "@/components/modules/Footer";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function App() {
   return (
     <ScrollArea>
-      {/* The Shadcn/ui Theme Provider */}
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        {/* Header Component */}
-        <Header />
+      {/* Header Component */}
+      <Header />
 
-        {/* Routes */}
-        <div className="mt-20 mb-6">
-          <Outlet />
-        </div>
+      {/* Routes */}
+      <div className="mt-20 mb-6">
+        <Outlet />
+      </div>
 
-        {/* Footer */}
-        <Footer />
-      </ThemeProvider>
+      {/* Footer */}
+      <Footer />
     </ScrollArea>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-
 import { routes } from "@/routes";
 import "./index.css";
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={routes} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
