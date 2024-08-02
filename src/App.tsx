74dc73fd9a02ea65e205342523810fef4cec5717
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-08-02 13:59:48 +0100

    Temp commit ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "@/components/modules/Header";
-import { Footer } from "@/components/modules/Footer";
+import { Header } from "@/components/modules/header";
+import { Footer } from "@/components/modules/footer";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function App() {
